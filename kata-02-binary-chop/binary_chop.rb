140b279aa277efb90d5898b5eeea8f2c8426bd13
Rob Perkins <perkinsjr.rob@gmail.com>
2015-02-22 14:49:01 -0500

    Iterative and recursive solutions

--- kata-02-binary-chop/binary_chop.rb
+++ kata-02-binary-chop/binary_chop.rb
@@ -1,7 +1,63 @@
 
 class BinaryChop
 
-  def self.chop(int, array_of_int)
+  # Solution 1 - Iterative
+  def self.chop1(int, array_of_int)
+    return -1 if array_of_int.size < 1
 
+    found_index = -1
+    array_of_int.each_with_index do |item, index|
+      if item == int
+        found_index = index
+        break
+      end
+    end
+
+    return found_index
+  end # Solution 1
+
+  # Solution 2 - Recursive
+  def self.chop(int, array_of_int)
+    return recursize_chop(int, array_of_int, 0)
+
+  end # Solution 2
+
+  private
+
+  def self.recursize_chop(int, array_of_int, overall_index_of_start)
+    # Easy checks up front
+    return -1 if array_of_int.size < 1
+
+    mid = array_of_int.size / 2
+    #puts "\nMid index (value): #{mid} (#{array_of_int[mid]})"
+
+    if array_of_int[mid] == int
+      #puts "I found you!"
+      return overall_index_of_start + mid
+
+    elsif array_of_int[mid] < int
+      #puts "Gotta go check with the big dogs"
+      new_array = array_of_int.slice(mid, array_of_int.size - 1)
+      #puts "Mid (#{array_of_int[mid]}) larger than given number (#{int}) -- New array: #{new_array}"
+      recursize_chop(
+        int,
+        new_array,
+        overall_index_of_start + mid)
+
+    elsif array_of_int[mid] > int
+      #puts "Gotta scale down and try again"
+      new_array = array_of_int.slice(0, mid)
+      #new_array = [] if new_array.nil? # If no items left in the array, return empty array
+      #puts "Mid (#{array_of_int[mid]}) smaller than given number (#{int}) -- New array: #{new_array}"
+      recursize_chop(
+        int,
+        new_array,
+        overall_index_of_start
+      )
+
+    else
+      return -1
+    end
   end
+
 end
--- kata-02-binary-chop/spec/binary_chop_spec.rb
+++ kata-02-binary-chop/spec/binary_chop_spec.rb
@@ -3,6 +3,9 @@ require 'rspec'
 require_rel '../binary_chop'
 
 describe BinaryChop do
+
+  BIG_ARRAY = (1..100000).step(2).to_a
+
   describe '#chop' do
 
     it 'returns -1 when passed an empty array' do
@@ -25,8 +28,8 @@ describe BinaryChop do
       expect(BinaryChop.chop(3, [1,3,5])).to eq(1)
     end
 
-    it 'returns 2 when integer is second item in odd numbered array' do
-      expect(BinaryChop.chop(2, [1,3,5])).to eq(2)
+    it 'returns 2 when integer is last item in odd numbered array' do
+      expect(BinaryChop.chop(5, [1,3,5])).to eq(2)
     end
 
     it 'returns -1 when integer is not in odd numbered multitem array' do
@@ -58,15 +61,15 @@ describe BinaryChop do
     end
 
     it 'returns 3 when integer is last item even numbered multitem array' do
-      expect(BinaryChop.chop(7, [1,3,5,7])).to eq(3)
+      expect(BinaryChop.chop(993, BIG_ARRAY)).to eq(496)
     end
 
     it 'returns -1 when integer is not in an even numbered multitem array' do
-      expect(BinaryChop.chop(0, [1,3,5,7])).to eq(-1)
-      expect(BinaryChop.chop(2, [1,3,5,7])).to eq(-1)
-      expect(BinaryChop.chop(4, [1,3,5,7])).to eq(-1)
-      expect(BinaryChop.chop(6, [1,3,5,7])).to eq(-1)
-      expect(BinaryChop.chop(8, [1,3,5,7])).to eq(-1)
+      expect(BinaryChop.chop(0, BIG_ARRAY)).to eq(-1)
+      expect(BinaryChop.chop(302, BIG_ARRAY)).to eq(-1)
+      expect(BinaryChop.chop(502, BIG_ARRAY)).to eq(-1)
+      expect(BinaryChop.chop(702, BIG_ARRAY)).to eq(-1)
+      expect(BinaryChop.chop(1000, BIG_ARRAY)).to eq(-1)
     end
 
   end
